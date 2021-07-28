83ad479ab66c22d30c477e561009d2f2da5a2d75
Rafael Ferreira <rafaelfdgyn@gmail.com>
2021-07-28 09:55:31 -0300

    challenge completed

--- src/modules/users/model/User.ts
+++ src/modules/users/model/User.ts
@@ -1,7 +1,20 @@
 import { v4 as uuidV4 } from "uuid";
 
 class User {
-  // Complete aqui
+  id?: string;
+  name: string;
+  admin: boolean;
+  email: string;
+  updated_at: Date;
+  created_at: Date;
+
+  constructor() {
+    if (!this.id) {
+      this.id = uuidV4();
+      this.admin = false;
+      this.created_at = new Date()
+    }
+  }
 }
 
 export { User };
